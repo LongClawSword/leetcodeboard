7a459dd1fc53042c1335b4f731df98001d3d2c03
Aryan <aryansingh7574@gmail.com>
2024-06-20 23:12:09 +0530

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-black py-6 sm:py-12">
-        <Button className=" bg-green-600">Click me</Button>
+        <Button className=" bg-green-600">Hello</Button>
       </div>
     </div>
   );
